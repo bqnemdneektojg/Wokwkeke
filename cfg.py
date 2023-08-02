9a38daf6b2d0249361ca9f096d9ac6c1953c2c38
bqnemdneektojg <141013246+bqnemdneektojg@users.noreply.github.com>
2023-08-02 15:20:27 +0300

    Add files via upload

---
+++ cfg.py
@@ -0,0 +1,9 @@
+# токен бота
+token = "6488327550:AAErLu6Due82w8UivfTzwrqFEU7KE0zexp0"
+# ID администратора
+admin_id = 11111111111111
+
+admin_username = "@admin_username"
+
+# лимит на вывод
+withdraw_limit = 100
